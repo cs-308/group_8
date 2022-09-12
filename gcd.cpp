c5271ef59b56946dc084f998bb8a2c8b75a355b8
cs308 <shaileshgrathod0812@gmail.com>
2022-09-12 00:04:31 +0000

    recursive gcd function added

--- gcd.cpp
+++ gcd.cpp
@@ -1,11 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int gcd(int a,int b){
-    int res=min(a,b);
-    while(res){
-        if(a%res==0 && b%res==0) return res;
+int recursive_gcd(int a, int b)
+{
+    return b == 0 ? a : recursive_gcd(b, a % b);
+}
+
+int gcd(int a, int b)
+{
+    int res = min(a, b);
+    while (res)
+    {
+        if (a % res == 0 && b % res == 0)
+            return res;
         res--;
     }
-
 }
